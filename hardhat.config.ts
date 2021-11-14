a220d936f2dd5e5cf09a4430f85accdcaf5676ae
soyboy <sbvegan@protonmail.com>
2021-11-13 16:28:26 -0800

    removing BaseRegistrar.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.7",
+  solidity: {
+    compilers:[{version: "0.8.4"}, {version: "0.8.7"}],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     hardhat: {
     },
